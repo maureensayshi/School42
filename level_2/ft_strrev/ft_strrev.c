ae2f835b485020569c0386c8acafb7b40cb47811
maureensayshi <maureen81711@gmail.com>
2018-07-12 19:37:22 -0700

    with_mian

---
+++ level_2/ft_strrev/ft_strrev.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miwang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/12 19:19:35 by miwang            #+#    #+#             */
+/*   Updated: 2018/07/12 19:36:49 by miwang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+
+int		ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char 	*ft_strrev(char *str)
+{
+	char tmp;
+	int	 len;
+	int  i;
+
+	len = ft_strlen(str);
+	i = 0;
+	while ( i < len / 2)
+	{
+		tmp = str[i];
+		str[i] = str[len - 1 - i];
+		str[len - 1 - i] = tmp;
+		i++;	
+	}
+	return (str);
+}
+
+
+int		main()
+{
+	char str[] = "hello world!";
+
+	printf("original: %s\n", str);
+	printf("rev: %s\n", ft_strrev(str));
+	return (0);
+}
